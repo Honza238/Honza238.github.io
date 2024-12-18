ba3ecf28d3b26235c1808336d42d32bcfcfe435c
Honza238 <71230224+Honza238@users.noreply.github.com>
2024-12-18 13:50:27 +0100

    script update

--- script.sh
+++ script.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
-# Set the source directory containing the images
+#Set the source dir
 echo Folder with images:
 read SOURCE_DIR
 
-# Set the output directory for metadata files
+#Set the output dir
 echo Metadata output folder:
 read OUTPUT_DIR
 
-# Ensure the output directory exists
 mkdir -p "$OUTPUT_DIR"
 
-# Loop through all image files in the source directory
+
 for image in "$SOURCE_DIR"/*.jpg; do
-    # Get the base name of the image without extension
+
     base_name=$(basename "$image" .jpg)
 
-    # Define the output metadata file
+    
     metadata_file="$OUTPUT_DIR/${base_name}_metadata.txt"
 
-    # Extract metadata using ExifTool
+    #Extract metadata using exiftool
     exiftool -s3 -ExposureTime -FNumber -ISO -FocalLength -LensModel -Model -FocalLengthIn35mmFormat "$image" > "$metadata_file"
     echo "$SOURCE_DIR/$base_name.jpg" >> $metadata_file
 
-    # Notify the user
     echo "Extracted metadata for $image to $metadata_file"
 
     ./ffmpeg -i $SOURCE_DIR/$base_name.jpg -vf "scale='if(gt(iw,ih),2000,-1):if(gt(ih,iw),2000,-1)'" -c:v libwebp -compression_level 6 $OUTPUT_DIR/${base_name}.webp
